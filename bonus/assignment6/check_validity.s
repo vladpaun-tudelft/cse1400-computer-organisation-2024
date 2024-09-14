a0c599f14b2c163736ecf7fa39ee6032e55d4c0b
vladpaun1 <vlad.paun06@gmail.com>
2024-09-14 17:06:28 +0200

    downloaded the specifications for parenthesis assignment

---
+++ bonus/assignment6/check_validity.s
@@ -0,0 +1,36 @@
+.text
+
+.include "basic.s"
+
+.global main
+
+# *******************************************************************************************
+# Subroutine: check_validity                                                                *
+# Description: checks the validity of a string of parentheses as defined in Assignment 6.   *
+# Parameters:                                                                               *
+#   first: the string that should be check_validity                                         *
+#   return: the result of the check, either "valid" or "invalid"                            *
+# *******************************************************************************************
+check_validity:
+	# prologue
+	pushq	%rbp 			# push the base pointer (and align the stack)
+	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+
+	# your code goes here
+
+	# epilogue
+	movq	%rbp, %rsp		# clear local variables from stack
+	popq	%rbp			# restore base pointer location 
+	ret
+
+main:
+	pushq	%rbp 			# push the base pointer (and align the stack)
+	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+
+	movq	$MESSAGE, %rdi		# first parameter: address of the message
+	call	check_validity		# call check_validity
+
+	popq	%rbp			# restore base pointer location 
+	movq	$0, %rdi		# load program exit code
+	call	exit			# exit the program
+
---
+++ bonus/assignment6/validParenthesesExamples/neighboringValid.s
@@ -0,0 +1,4 @@
+.text
+
+MESSAGE: .string "<>{}[]()"
+
