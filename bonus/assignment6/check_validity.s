bb845607b0122daabc266b487e845a78a3608862
vladpaun1 <vlad.paun06@gmail.com>
2024-09-16 15:42:37 +0200

    finished Parenthesis

--- bonus/assignment6/check_validity.s
+++ bonus/assignment6/check_validity.s
@@ -1,9 +1,9 @@
 .text
 
-succes_out: .asciz "\nProgram terminated succesfully. String is valid\n"
-fail_out: .asciz "\nProgram failed. String is invalid\n"
+succes_out: .asciz "valid"
+fail_out: .asciz "invalid"
 
-.include "validParenthesesExamples/basic.s"
+.include "basic.s"
 
 .global main
 
@@ -18,6 +18,90 @@ check_validity:
 	# prologue
 	pushq	%rbp 			# push the base pointer (and align the stack)
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+
+	pushq $9000				# Push a control value to check if we ve closed all brackets at the end
+	subq $8, %rsp			# Keep stack 16-aligned
+
+	subq $1, %rdi			# the loop increments rdi at the beginning, so subtract one now so it d fine
+
+	start:
+	addq $1, %rdi			# Increment rdi, which means move to the next character
+
+	movq $0, %rax			# Clear rax
+	movb (%rdi), %al		# Move the current character at rdi, into the least segnificant byte of rax. Our characters ascii is now in rax
+
+	# Case for null character
+	cmpq $0, %rax			# If it's the null character, we've reached the end of the string, end the loop		
+	je end_loop
+
+	# Cases for open brackets
+	cmpq $40, %rax			# If it's '(', push it
+	je push_case
+
+	cmpq $60, %rax			# If it's '<', push it
+	je push_case
+
+	cmpq $91, %rax			# If it's '[', push it
+	je push_case
+
+	cmpq $123, %rax			# If it's '{', push it
+	je push_case
+
+	# Cases for cosed brackets
+	cmpq $41, %rax			# If it's ')', pop the last pushed thing and compare
+	je pop_case
+
+	cmpq $62, %rax			# If it's '>', pop the last pushed thing and compare
+	je pop_case
+
+	cmpq $93, %rax			# If it's ']', pop the last pushed thing and compare
+	je pop_case
+
+	cmpq $125, %rax			# If it's '}', pop the last pushed thing and compare
+	je pop_case
+
+	# If it's not an open or closed bracket, just move to next itieration
+	jmp start
+
+	push_case:				# Push the character, keep stack aligned, then continue to next iteration
+		pushq %rax
+		subq $8, %rsp
+		jmp start
+
+	pop_case:				# Compare the current character with the last pushed open bracked. Check if they are a valid pair
+		addq $8, %rsp
+		popq %rcx
+
+		subq %rcx, %rax		# Subtract the ASCII values of the 2 characters
+
+		cmpq $2, %rax		# If the difference between them is larger than 2, it is an invalid pair, return invalid
+		jg error
+
+		cmp $0, %rax		# If the difference between them is negative, it is an invalid pair, return invalid
+		jl error
+
+		jmp start
+
+	end_loop:
+	
+	# At the start we pushed a control value.
+		# Pop one more time and check if we are back at the control value. If we are not, there are still unclosed brackets.
+	addq $8, %rsp			
+	popq %rcx
+	cmpq $9000, %rcx
+	jne error
+
+	# return 'valid'
+	movq $succes_out, %rax
+
+	jmp return
+
+	error:
+
+	# Return 'invalid'
+	movq $fail_out, %rax
+
+	jmp return
 
 	return:
 	# epilogue
